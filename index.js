f72235d6cf2bd69ac0641fbeda0768e9a2f59119
rfloresj <txlinemusic@gmail.com>
2023-07-30 23:15:59 -0600

    create-the-unordered-list

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-let myLeads = [];
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById('input-btn');
+const ulEl = document.getElementById('ul-el');
+
+console.log(ulEl)
 
 inputBtn.addEventListener("click", () => {
 myLeads.push(inputEl.value);
 console.log(myLeads);
 }
 );
+
+for(let i = 0; i < myLeads.length; i++){
+    console.log(myLeads[i]);
+}
