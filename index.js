4c4c217ec47bd73278655a8bbb7227a79ca5d474
rfloresj <txlinemusic@gmail.com>
2023-07-31 23:24:30 -0600

    render-li-innerHTML

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
-const inputBtn = document.getElementById('input-btn');
-const ulEl = document.getElementById('ul-el');
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
-console.log(ulEl)
+console.log(ulEl);
 
 inputBtn.addEventListener("click", () => {
-myLeads.push(inputEl.value);
-console.log(myLeads);
-}
-);
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
+});
 
-for(let i = 0; i < myLeads.length; i++){
-    console.log(myLeads[i]);
+for (let i = 0; i < myLeads.length; i++) {
+  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
 }
