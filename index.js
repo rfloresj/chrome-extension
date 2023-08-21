1b15afdbdda166c91b0a7d834e19757a9d2ce392
rfloresj <txlinemusic@gmail.com>
2023-08-20 19:07:04 -0600

    checking-localstorage-before-rendering

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-localStorage.clear();
+//localStorage.clear();
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(myLeads)
 
-console.log(leadsFromLocalStorage);
+if(leadsFromLocalStorage){
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
 
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value);
