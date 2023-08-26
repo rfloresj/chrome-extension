acb116999f8e8491d8b0a1443847215c080f4a22
rflores <104962825+rfloresj@users.noreply.github.com>
2023-08-26 15:48:13 -0600

    create-the-tabBtn

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById('tab-btn');
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   render(myLeads);
 }
+
+const tabs = [
+  {url: "https://www.amazon.com"}
+];
+
+tabBtn.addEventListener("click", () => {
+  console.log(tabs[0].url);
+}) 
 
 function render(leads) {
   let listItems = "";
